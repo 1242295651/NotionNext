541cc557fdd7b2ff182561ee3b9b43bdccbb535b
1242295651 <42509084+1242295651@users.noreply.github.com>
2022-10-27 16:15:53 +0800

    Update blog.config.js
    add id

--- blog.config.js
+++ blog.config.js
@@ -4,8 +4,8 @@ const BLOG = {
   BIO: '一个普通的干饭人🍚', // 作者简介
   LINK: 'https://tangly1024.com', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
-  NOTION_PAGE_ID:https://pointy-sunday-13c.notion.site/4cec7f33c8d14adf8ffed287a859578c?v=33f7bb6c0679424097aaf00eb242ec7e
-  process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
+  NOTION_PAGE_ID:
+  process.env.NOTION_PAGE_ID || '6620127e0a7f4506b8fa7c1443f7f500', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
   DEBUG: process.env.NEXT_PUBLIC_DEBUG || false, // 是否显示调试按钮
 
